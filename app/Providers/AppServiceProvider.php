8d1b1d605e525aa619e06444bf29efc8ece54768
adenadams20 <adamsdiene@gmail.com>
2026-03-19 07:10:12 +0000

    update https-4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Importation nécessaire
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Force le HTTPS uniquement en production (sur Railway)
+        if (!app()->isLocal()) {
+        URL::forceScheme('https');
+    }
     }
 }
